1e6795705391ff419653d0e57a7b633bbe0a9cb1
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2022-07-02 13:03:45 +0200

    Update custom_app_review.rb

--- custom_files/custom_app_review.rb
+++ custom_files/custom_app_review.rb
@@ -9,23 +9,6 @@ module AresMUSH
       if (traits_msg != "") && (traits_msg != "")
          msg = msg + "%r%xh%xr* " + traits_msg + "%xn"
       end
-
-      if ( Marque.is_adept(char) && ( !Marque.marque_value(char) || Marque.marque_value(char) == "" ) )
-         marque_msg = t('custom.no_marque_set')
-         msg = msg + "%r%xh%xr* #{marque_msg} %xn"
-      end
-            
-      scion_power = (char.fs3_advantages.find(name: "Scion Power").first)
-      if (scion_power) && (!char.groups["scion"] || (char.groups["scion"] == "None") || (char.groups["scion"] == "")) 
-         scion_msg = t('custom.no_scion_set')
-         msg = msg + "%r%xh%xr* #{scion_msg} %xn"
-      end
-      
-      connections = (char.fs3_advantages.find(name: "Connections").first)
-      if ((connections) && (!(char.connections) || (char.connections == "")))
-         con_msg = t('custom.no_connections_set')
-         msg = msg + "%r%xh%xr* #{con_msg} %xn"
-      end
  
       if (msg == "")
          msg = t('chargen.ok')
