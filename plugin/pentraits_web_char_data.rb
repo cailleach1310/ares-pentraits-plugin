11ee1cb9540b8c985ae23039ca258bd9bbea3495
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2022-07-04 18:24:40 +0200

    Update pentraits_web_char_data.rb

--- plugin/pentraits_web_char_data.rb
+++ plugin/pentraits_web_char_data.rb
@@ -3,8 +3,10 @@ module AresMUSH
     class WebTraitsDataBuilder
       def build(char, viewer)
         is_owner = (viewer && viewer.id == char.id)
-        
-        show_traits = PenTraits.can_manage_traits?(viewer) || is_owner
+
+        visible = Global.read_config("pentraits","trait_visibility")
+
+        show_traits = visible || PenTraits.can_manage_traits?(viewer) || is_owner
         
         if show_traits
            return get_trait_list(char.pen_traits)
