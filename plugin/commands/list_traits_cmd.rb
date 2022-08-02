ab93641841049799e996997e465df860bae75ade
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2022-08-02 16:57:59 +0200

    Update list_traits_cmd.rb

--- plugin/commands/list_traits_cmd.rb
+++ plugin/commands/list_traits_cmd.rb
@@ -4,6 +4,9 @@ module AresMUSH
       include CommandHandler
 
       def handle
+        if PenTraits.not_set?(enactor)
+           PenTraits.init_traits(enactor)
+        end
         template = TraitsTemplate.new enactor
 	client.emit template.render
       end
